2b1bbd5fde60bef7b7bf3568314c5195e43d8385
Victor <victor@Robin.local>
2025-10-01 18:37:36 -0400

    Initial commit

---
+++ app/src/main/java/ucne/edu/Victor_Ferreiras_AP2_P1/MainActivity.kt
@@ -0,0 +1,47 @@
+package ucne.edu.Victor_Ferreiras_AP2_P1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ucne.edu.Victor_Ferreiras_AP2_P1.ui.theme.PrimerParcialTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PrimerParcialTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PrimerParcialTheme {
+        Greeting("Android")
+    }
+}
