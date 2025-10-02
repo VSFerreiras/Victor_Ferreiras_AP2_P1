c1d557053a24a91b33c650e1c05953836ce32467
victor <vsferreiras402@protonmail.com>
2025-10-01 21:10:19 -0400

    Creado UpsertEntradaUseCase.kt

---
+++ app/src/main/java/ucne/edu/Victor_Ferreiras_AP2_P1/domain/usecase/UpsertEntradaUseCase.kt
@@ -0,0 +1,13 @@
+package ucne.edu.Victor_Ferreiras_AP2_P1.domain.usecase
+
+import ucne.edu.Victor_Ferreiras_AP2_P1.domain.model.Entrada
+import ucne.edu.Victor_Ferreiras_AP2_P1.domain.repository.EntradaRepository
+import javax.inject.Inject
+
+class UpsertEntradaUseCase @Inject constructor(
+    private val repository: EntradaRepository
+) {
+    suspend operator fun invoke(entrada: Entrada) {
+        repository.upsert(entrada)
+    }
+}
