1b31170524320e92dfd5644643887d2c70db3035
victor <vsferreiras402@protonmail.com>
2025-10-01 21:11:18 -0400

    Entrada model

---
+++ app/src/main/java/ucne/edu/Victor_Ferreiras_AP2_P1/domain/model/Entrada.kt
@@ -0,0 +1,9 @@
+package ucne.edu.Victor_Ferreiras_AP2_P1.domain.model
+
+data class Entrada(
+    val id: Int = 0,
+    val fecha: String = "",
+    val nombreCliente: String = "",
+    val cantidad: Int = 0,
+    val precio: Double = 0.0
+)
