7e2a0237bc48fef65cd0ba74d6705df3e18ab68a
victor <vsferreiras402@protonmail.com>
2025-10-01 21:11:42 -0400

    Entrada repository creado usando mappers

---
+++ app/src/main/java/ucne/edu/Victor_Ferreiras_AP2_P1/data/EntradaRepositoryImpl.kt
@@ -0,0 +1,34 @@
+package ucne.edu.Victor_Ferreiras_AP2_P1.data.repository
+
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import ucne.edu.Victor_Ferreiras_AP2_P1.data.EntradaDao
+import ucne.edu.Victor_Ferreiras_AP2_P1.data.mappers.toDomain
+import ucne.edu.Victor_Ferreiras_AP2_P1.data.mappers.toEntity
+import ucne.edu.Victor_Ferreiras_AP2_P1.domain.model.Entrada
+import ucne.edu.Victor_Ferreiras_AP2_P1.domain.repository.EntradaRepository
+import javax.inject.Inject
+
+class EntradaRepositoryImpl @Inject constructor(
+    private val dao: EntradaDao
+) : EntradaRepository {
+    override suspend fun upsert(entrada: Entrada) {
+        if (entrada.id == 0) {
+            dao.insert(entrada.toEntity())
+        } else {
+            dao.update(entrada.toEntity())
+        }
+    }
+
+    override fun observeAll(): Flow<List<Entrada>> {
+        return dao.observeAll().map { list -> list.map { it.toDomain() } }
+    }
+
+    override suspend fun getById(id: Int): Entrada? {
+        return null
+    }
+
+    override suspend fun delete(entrada: Entrada) {
+        dao.delete(entrada.toEntity())
+    }
+}
