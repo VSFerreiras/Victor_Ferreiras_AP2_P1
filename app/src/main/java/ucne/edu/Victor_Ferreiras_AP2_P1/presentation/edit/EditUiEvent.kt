ebdc0b070b58b2ece2b4aad93c07077f8a0de0db
VSFerreiras <vsferreiras402@protonmail.com>
2025-10-06 17:39:01 -0400

    Mejorado edit ui event screen para presentar multiples errores a la vez

--- app/src/main/java/ucne/edu/Victor_Ferreiras_AP2_P1/presentation/edit/EditUiEvent.kt
+++ app/src/main/java/ucne/edu/Victor_Ferreiras_AP2_P1/presentation/edit/EditUiEvent.kt
@@ -2,5 +2,4 @@ package ucne.edu.Victor_Ferreiras_AP2_P1.presentation.edit
 
 sealed class EditUiEvent {
     object Saved : EditUiEvent()
-    data class ShowError(val message: String) : EditUiEvent()
 }
